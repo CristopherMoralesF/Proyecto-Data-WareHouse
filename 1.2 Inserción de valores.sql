8176a06afab205d990bbad6e968fd20acd255f25
CristopherMoralesF <71911123+CristopherMoralesF@users.noreply.github.com>
2022-07-19 22:53:58 -0500

    Cris 19072022 V2
    Validation of file 1.2, some corrections in some table inserts, I delete all the information that is duplicated from file 1.1.
    Please do not duplicate the information again.
    
    @sleonc0217 review the commends added in the file and perform the corrections or delete the lines as you consider.

--- 1.2 Inserción de valores.sql
+++ 1.2 Inserción de valores.sql
@@ -4,343 +4,143 @@ GO
 /* INICIAR LA INSERCIÓN DE LOS VALORES DE CADA TABLA */
 
 
-INSERT INTO FARMACIA
-VALUES(1,'Farmacia La Arboleda',2000,'Cartago', 'Guadalupe',12);
+INSERT INTO FARMACIA VALUES
+	(2,'Farmacia La Arboleda',2000,'Cartago, Guadalupe',12),
+	(3,'Farmacia La Arboleda',3000,'San Jose, Curridabat',5),
+	(4,'Farmacia La Arboleda',1000,'Heredia, San Francisco',8),
+	(5,'Farmacia La Arboleda',2500,'Alajuela, Coyol',3)
+GO
+
+/*
+-----> Cris comment: agregar valores distintos al archivo 1.1, si los valores son duplicados los scripts no corren por el PK y los FK <-----
+
+INSERT INTO CATEGORIA_MEDICAMENTO VALUES 
+	(1,'Analgésicos','Fármacos que tienen como finalidad aliviar el dolor físico'),
+	(2,'Antialérgicos','Fármacos que tienen la finalidad de combatir los efectos negativos de las reacciones alérgicas'),
+	(3,'Antiinflamatorios','Fármacos que tienen como finalidad reducir los efectos de la inflamación'),
+	(4,'Antiinfecciosos','Este tipo de medicamentos están recetados para hacer frente a infecciones'),
+	(5,'Antipiréticos','Este tipo de medicamentos que tienen la capacidad de reducir la fiebre')
+GO
+*/
 
 
+INSERT INTO PROVEEDOR VALUES 
+	(4, 'Pfizer', 3, '2020-02-23', 4),
+	(5, 'Astrazeneca', 2, '2021-07-05', 6),
+	(6, 'Johnson & Johnson', 4, '2019-10-17', 10),
+	(7, 'gsk', 6, '2010-01-20', 6)
+GO
 
-VALUES(2,'Farmacia La Arboleda',3000,'San Jose', 'Curridabat',5);
-VALUES(3,'Farmacia La Arboleda',1000,'Heredia', 'San Francisco',8);
-VALUES(4,'Farmacia La Arboleda',2500,'Alajuela', 'Coyol',3);
+INSERT INTO PERSONA VALUES
+	(21,'Julian', 'Francisco','Miranda','Castillo',113543203,'1990-06-27'),
+	(22,'Juan', 'Luis','Lopez','Rodriguez',524526384,'1987-03-18'),
+	(23,'Raquel', 'Cristina','Mora','Cascante',112437656,'1997-04-18'),
+	(24,'Krista', 'Isabell','Leon','Cascante',156095425,'1999-06-11'),
+	(25,'Marta', 'Patricia','Calderon','Guardia',124826184,'2000-07-03'),
+	(26,'Ronald', 'jose','Roman','Figueres',173455234,'1998-03-18'),
+	(27,'Sofia', 'Lucia','Jimenez','Ortiz',162323384,'2001-05-04'),
+	(28,'Ileana', '','Diaz','Fernandez',173527432,'1987-03-18'),
+	(29,'Marina', '','Loza','Vindas',187365294,'1992-06-11'),
+	(30,'Miranda', '','Aguilar','Mesa',163527532,'2000-12-20'),
+	(31,'Fiorella', 'Milagro','Mata','Lobo',132743821,'1976-08-16'),
+	(32,'Gabriel', 'Josue','Escalante','Abarca',189346278,'1984-05-27'),
+	(33,'Fabian', 'Andres','Barquero','Phillips',143258756,'1993-11-17'),
+	(34,'Valeria', '','Sancho','Leandro',173530954,'1995-05-12'),
+	(35,'Natalia', '','Sancho','Leandro',173530955,'1995-05-12'),
+	(36,'Fancy', '','Campos','Sanchez',165348723,'2000-09-29'),
+	(37,'David', 'Alberto','Campos','Sanchez',183623483,'2000-02-18'),
+	(38,'Raul', '','Salas','Piedra',173346955,'2002-03-18'),
+	(39,'Mariana', 'Rebeca','Quesada','Vargas',239648942,'2003-01-30'),
+	(40,'Sebastian', '','Valverde','Leandro',179838552,'1995-12-01'),
+	(41,'Luis', 'Fernando','Meneses','Sancho',182632452,'1976-07-29'),
+	(42,'Bryan', '','Fernandez','Castro',176230352,'1985-11-28'),
+	(43,'Jose', '','Ubisco','Santamaria',179803552,'1984-08-17'),
+	(44,'Carlos', '','Martinez','Mora',170938552,'1956-12-08'),
+	(45,'Javier', 'Fabricio','Bosque','Lobo',179838507,'2002-08-16'),
+	(46,'Rodolfo', '','Naranjo','Sevilla',179838501,'2003-02-23'),
+	(47,'Fausto', '','Costa','Salvador',179830852,'2001-12-01'),
+	(48,'Dominic', '','Castilla','Leon',179012552,'2003-04-27'),
+	(49,'Pablo', 'Luciano','Castro','Mesa',189002852,'1959-12-01'),
+	(50,'Steven', 'Ricardo','Guerra','Tenorio',179000552,'1959-11-14')
+GO
 
+INSERT INTO EMPLEADO VALUES
+	(8,8,'2017-05-01',2),
+	(9,9,'2017-05-01',2),
+	(10,10,'2018-10-03',2),
+	(11,11,'2019-02-01',2),
+	(12,12,'2018-08-10',2),
+	(13,13,'2017-05-01',2),
+	(14,14,'2014-05-01',3),
+	(15,15,'2014-02-01',3),
+	(16,16,'2014-06-04',3),
+	(17,17,'2015-01-10',3),
+	(18,18,'2019-01-01',4),
+	(19,19,'2019-03-02',4),
+	(20,20,'2019-01-04',4),
+	(21,21,'2020-05-06',4)
+GO
 
-INSERT INTO CATEGORIA_MEDICAMENTO
-VALUES (1,'Analgésicos','Fármacos que tienen como finalidad aliviar el dolor físico');
-INSERT INTO CATEGORIA_MEDICAMENTO
-VALUES (2,'Antialérgicos','Fármacos que tienen la finalidad de combatir los efectos negativos de las reacciones alérgicas');
-INSERT INTO CATEGORIA_MEDICAMENTO
-VALUES (3,'Antiinflamatorios','Fármacos que tienen como finalidad reducir los efectos de la inflamación');
-INSERT INTO CATEGORIA_MEDICAMENTO
-VALUES (4,'Antiinfecciosos','Este tipo de medicamentos están recetados para hacer frente a infecciones');
-
-VALUES (5,'Antipiréticos','Este tipo de medicamentos que tienen la capacidad de reducir la fiebre');
-
-
-
-
-INSERT INTO PROVEEDOR 
-VALUES (1, 'Fischel',4,'2019-09-02',8);
-INSERT INTO PROVEEDOR
-VALUES (2, 'Farmanova',2,'2021-06-09',7);
-INSERT INTO PROVEEDOR
-VALUES (3, 'CEFA', 1, '2022-03-15', 6);
-
-INSERT INTO PROVEEDOR
-VALUES (4, 'Pfizer', 3, '2020-02-23', 4);
-INSERT INTO PROVEEDOR
-VALUES (5, 'Astrazeneca', 2, '2021-07-05', 6);
-INSERT INTO PROVEEDOR
-VALUES (6, 'Johnson & Johnson', 4, '2019-10-17', 10);
-INSERT INTO PROVEEDOR
-VALUES (7, 'gsk', 6, '2010-01-20', 6);
-
-
-INSERT INTO PERSONA
-VALUES(1,'Mariangel', '','Bustamante', 'Zúñiga',154895894,'1991-04-07');
-INSERT INTO PERSONA
-VALUES(2,'Fernanda', '','Fernández', 'Quiros',133082681,'1983-02-10');
-INSERT INTO PERSONA
-VALUES(3,'Juan','Carlos','López', 'Fernández',267875490,'1996-10-16');
-INSERT INTO PERSONA
-VALUES(4,'Roberto', '','Rodriguez', 'Salazar',348765894,'2001-09-29');
-INSERT INTO PERSONA
-VALUES(5,'Markel', '','Rosales', 'Villalobos',201574875,'1982-11-23');
-INSERT INTO PERSONA
-VALUES(6,'Jacqueline', '','Vera', 'Brenes',118516075,'1990-02-10');
-INSERT INTO PERSONA
-VALUES(7,'Denis', '','Vázquez', 'Gutiérrez',378571054,'1983-12-16');
-INSERT INTO PERSONA
-VALUES(8,'Sofía', '','Corrales','García',129061129,'1995-01-21');
-INSERT INTO PERSONA
-VALUES(9,'Jose', '','Castro','Alfaro',987667718,'1979-02-22');
-INSERT INTO PERSONA
-VALUES(10,'Jorge', '','López','Vega',805114074,'1980-01-11');
-INSERT INTO PERSONA
-VALUES(11,'Víctor', '','Araya','Aguilar',345829764,'1994-11-23');
-INSERT INTO PERSONA
-VALUES(12,'Rosa', '','Solano','Calderón',832899855,'1992-05-08');
-INSERT INTO PERSONA
-VALUES(13,'Andrea', '','Alvarado','Valverde',312862968,'1972-08-07');
-INSERT INTO PERSONA
-VALUES(14,'Álvaro', '','Chaves','Chavarría',575565762,'1991-07-09');
-INSERT INTO PERSONA
-VALUES(15,'David', '','Pérez','Castillo',362525060,'1990-04-18');
-INSERT INTO PERSONA
-VALUES(16,'Katherine', '','Morales','Salas',708243699,'1974-09-15');
-INSERT INTO PERSONA
-VALUES(17,'Edwin', '','Campos','Solís',924123449,'1987-07-25');
-INSERT INTO PERSONA
-VALUES(18,'Walter', '','Quesada','Granados',955721176,'1992-07-01');
-INSERT INTO PERSONA
-VALUES(19,'Diego', '','Gómez','Chinchilla',926531809,'1980-07-25');
-INSERT INTO PERSONA
-VALUES(20,'Jessica', '','Arias','Camacho',434083875,'1966-04-01');
-
-INSERT INTO PERSONA
-VALUES(21,'Julian', 'Francisco','Miranda','Castillo',113543203,'1990-06-27');
-INSERT INTO PERSONA
-VALUES(22,'Juan', 'Luis','Lopez','Rodriguez',524526384,'1987-03-18');
-INSERT INTO PERSONA
-VALUES(23,'Raquel', 'Cristina','Mora','Cascante',112437656,'1997-04-18');
-INSERT INTO PERSONA
-VALUES(24,'Krista', 'Isabell','Leon','Cascante',156095425,'1999-06-11');
-INSERT INTO PERSONA
-VALUES(25,'Marta', 'Patricia','Calderon','Guardia',124826184,'2000-07-03');
-INSERT INTO PERSONA
-VALUES(26,'Ronald', 'jose','Roman','Figueres',173455234,'1998-03-18');
-INSERT INTO PERSONA
-VALUES(27,'Sofia', 'Lucia','Jimenez','Ortiz',162323384,'2001-05-04');
-INSERT INTO PERSONA
-VALUES(28,'Ileana', '','Diaz','Fernandez',173527432,'1987-03-18');
-INSERT INTO PERSONA
-VALUES(29,'Marina', '','Loza','Vindas',187365294,'1992-06-11');
-INSERT INTO PERSONA
-VALUES(30,'Miranda', '','Aguilar','Mesa',163527532,'2000-12-20');
-INSERT INTO PERSONA
-VALUES(31,'Fiorella', 'Milagro','Mata','Lobo',132743821,'1976-08-16');
-INSERT INTO PERSONA
-VALUES(32,'Gabriel', 'Josue','Escalante','Abarca',189346278,'1984-05-27');
-INSERT INTO PERSONA
-VALUES(33,'Fabian', 'Andres','Barquero','Phillips',143258756,'1993-11-17');
-INSERT INTO PERSONA
-VALUES(34,'Valeria', '','Sancho','Leandro',173530954,'1995-05-12');
-INSERT INTO PERSONA
-VALUES(35,'Natalia', '','Sancho','Leandro',173530955,'1995-05-12');
-INSERT INTO PERSONA
-VALUES(36,'Fancy', '','Campos','Sanchez',165348723,'2000-09-29');
-INSERT INTO PERSONA
-VALUES(37,'David', 'Alberto','Campos','Sanchez',183623483,'2000-02-18');
-INSERT INTO PERSONA
-VALUES(38,'Raul', '','Salas','Piedra',173346955,'2002-03-18');
-INSERT INTO PERSONA
-VALUES(39,'Mariana', 'Rebeca','Quesada','Vargas',239648942,'2003-01-30');
-INSERT INTO PERSONA
-VALUES(40,'Sebastian', '','Valverde','Leandro',179838552,'1995-12-01');
-INSERT INTO PERSONA
-VALUES(41,'Luis', 'Fernando','Meneses','Sancho',182632452,'1976-07-29');
-INSERT INTO PERSONA
-VALUES(42,'Bryan', '','Fernandez','Castro',176230352,'1985-11-28');
-INSERT INTO PERSONA
-VALUES(43,'Jose', '','Ubisco','Santamaria',179803552,'1984-08-17');
-INSERT INTO PERSONA
-VALUES(44,'Carlos', '','Martinez','Mora',170938552,'1956-12-08');
-INSERT INTO PERSONA
-VALUES(45,'Javier', 'Fabricio','Bosque','Lobo',179838507,'2002-08-16');
-INSERT INTO PERSONA
-VALUES(46,'Rodolfo', '','Naranjo','Sevilla',179838501,'2003-02-23');
-INSERT INTO PERSONA
-VALUES(47,'Fausto', '','Costa','Salvador',179830852,'2001-12-01');
-INSERT INTO PERSONA
-VALUES(48,'Dominic', '','Castilla','Leon',179012552,'2003-04-27');
-INSERT INTO PERSONA
-VALUES(49,'Pablo', 'Luciano','Castro','Mesa',189002852,'1959-12-01');
-INSERT INTO PERSONA
-VALUES(50,'Steven', 'Ricardo','Guerra','Tenorio',179000552,'1959-11-14');
-
-INSERT INTO EMPLEADO
-VALUES(1,1,'2011-06-09',1);
-INSERT INTO EMPLEADO
-VALUES(2,2,'2013-11-15',1);
-INSERT INTO EMPLEADO
-VALUES(3,3,'2016-10-29',1);
-INSERT INTO EMPLEADO
-VALUES(4,4,'2022-04-03',1);
-INSERT INTO EMPLEADO
-VALUES(5,5,'2012-01-11',1);
-INSERT INTO EMPLEADO
-VALUES(6,6,'2010-12-22',1);
-INSERT INTO EMPLEADO
-VALUES(7,7,'2013-08-01',1);
-
-INSERT INTO EMPLEADO
-VALUES(8,8,'2017-05-01',2);
-INSERT INTO EMPLEADO
-VALUES(9,9,'2017-05-01',2);
-INSERT INTO EMPLEADO
-VALUES(10,10,'2018-10-03',2);
-INSERT INTO EMPLEADO
-VALUES(11,11,'2019-02-01',2);
-INSERT INTO EMPLEADO
-VALUES(12,12,'2018-08-10',2);
-INSERT INTO EMPLEADO
-VALUES(13,13,'2017-05-01',2);
-
-INSERT INTO EMPLEADO
-VALUES(14,14,'2014-05-01',3);
-INSERT INTO EMPLEADO
-VALUES(15,15,'2014-02-01',3);
-INSERT INTO EMPLEADO
-VALUES(16,16,'2014-06-04',3);
-INSERT INTO EMPLEADO
-VALUES(17,17,'2015-01-10',3);
-
-INSERT INTO EMPLEADO
-VALUES(18,18,'2019-01-01',4);
-INSERT INTO EMPLEADO
-VALUES(19,19,'2019-03-02',4);
-INSERT INTO EMPLEADO
-VALUES(20,20,'2019-01-04',4);
-INSERT INTO EMPLEADO
-VALUES(21,21,'2020-05-06',4);
+INSERT INTO CLIENTES VALUES
+	(9,22,'2015-04-18'),
+	(10,23,'2015-04-18'),
+	(11,24,'2015-04-18'),
+	(12,25,'2015-04-18'),
+	(13,26,'2015-04-18'),
+	(14,27,'2015-04-18'),
+	(15,28,'2015-04-18'),
+	(16,29,'2015-04-18'),
+	(17,30,'2015-04-18'),
+	(18,31,'2015-04-18'),
+	(19,32,'2015-04-18'),
+	(20,33,'2015-04-18'),
+	(21,34,'2015-04-18'),
+	(22,35,'2015-04-18'),
+	(23,36,'2015-04-18'),
+	(24,37,'2015-04-18'),
+	(25,38,'2015-04-18'),
+	(26,39,'2015-04-18'),
+	(27,40,'2015-04-18')
+GO
 
 
 
 
+INSERT INTO MEDICAMENTO VALUES 
+	(6,'Dipirona','Es un potente analgésico',18,'2023-10-17',1,2),
+	(7,'Nefopam','Se usa para tratar el dolor moderado a intenso.',13,'2023-08-02',1,2),
+	(8,'Loratadina','Alivia los síntomas asociados a la rinitis alérgica',25,'2023-09-30',2,2)
+GO
+
+/*
+-----> Cris comment: Al parecer no existe la FK 28 y  29, crearlas o cambiarlas en el query a partir de acá, todas las tablas tienen el mismo issue <-----
+*/
+
+INSERT INTO DISTRIBUIDOR VALUES
+	(6,27,'2019-10-20',6,6),	
+	(7,28,'2022-04-01',7,7),	
+	(8,29,'2010-01-23',8,7)
+GO
 
 
+INSERT INTO RECETA_MEDICA VALUES
+	(6,'Tomar de 1 a 2 g cada 8 horas',1,39,'2020-08-08'),
+	(7,'Inicialmente, 60 mg pero puede variar de 30-90 mg tid. Máx.: 300 mg diarios',1,40,'2021-08-08'),
+	(8,'Dos cucharaditas de LORATADINA (10 ml = 10 mg) una vez al día.',1,35,'2019-01-03')
+GO
 
-INSERT INTO CLIENTES
-VALUES(1,8,'2016-01-21');
-INSERT INTO CLIENTES
-VALUES(2,9,'2010-02-22');
-INSERT INTO CLIENTES
-VALUES(3,10,'2011-01-11');
-INSERT INTO CLIENTES
-VALUES(4,11,'2014-11-23');
-INSERT INTO CLIENTES
-VALUES(5,12,'2019-05-08');
-INSERT INTO CLIENTES
-VALUES(6,13,'2020-08-07');
-INSERT INTO CLIENTES
-VALUES(7,14,'2021-07-09');
-INSERT INTO CLIENTES
-VALUES(8,15,'2015-04-18');
+INSERT INTO RECETA_X_MEDICAMENTOS VALUES
+	(6,6,6),
+	(7,7,7),
+	(8,8,8)
+GO
 
-INSERT INTO CLIENTES
-VALUES(9,22,'2015-04-18');
-INSERT INTO CLIENTES
-VALUES(10,23,'2015-04-18');
-INSERT INTO CLIENTES
-VALUES(11,24,'2015-04-18');
-INSERT INTO CLIENTES
-VALUES(12,25,'2015-04-18');
-INSERT INTO CLIENTES
-VALUES(13,26,'2015-04-18');
-INSERT INTO CLIENTES
-VALUES(14,27,'2015-04-18');
-INSERT INTO CLIENTES
-VALUES(15,28,'2015-04-18');
-INSERT INTO CLIENTES
-VALUES(16,29,'2015-04-18');
-INSERT INTO CLIENTES
-VALUES(17,30,'2015-04-18');
-INSERT INTO CLIENTES
-VALUES(18,31,'2015-04-18');
-INSERT INTO CLIENTES
-VALUES(19,32,'2015-04-18');
-INSERT INTO CLIENTES
-VALUES(20,33,'2015-04-18');
-INSERT INTO CLIENTES
-VALUES(21,34,'2015-04-18');
-INSERT INTO CLIENTES
-VALUES(22,35,'2015-04-18');
-INSERT INTO CLIENTES
-VALUES(23,36,'2015-04-18');
-INSERT INTO CLIENTES
-VALUES(24,37,'2015-04-18');
-INSERT INTO CLIENTES
-VALUES(25,38,'2015-04-18');
-INSERT INTO CLIENTES
-VALUES(26,39,'2015-04-18');
-INSERT INTO CLIENTES
-VALUES(27,40,'2015-04-18');
-
-
-
-
-INSERT INTO MEDICAMENTO
-VALUES (1,'Paracetamol','Eficaz para el control del dolor leve o moderado causado por afecciones articulares',32,'2023-09-24',1,1);
-INSERT INTO MEDICAMENTO
-VALUES (2,'Vicoprofen','Medicamento utilizado para tratar los síntomas de dolor agudo',17,'2023-04-10',1,1);
-INSERT INTO MEDICAMENTO
-VALUES (3,'Cetirizina','Fármaco utilizado para el tratamiento de la alergia',20,'2023-06-03',2,1);
-INSERT INTO MEDICAMENTO
-VALUES (4,'Fenoprofeno','Es un fármaco antiinflamatorio no esteroideo',10,'2023-05-28',3,1);
-INSERT INTO MEDICAMENTO
-VALUES (5,'Amoxicilina','Tratamiento de las infecciones producidas por microorganismos sensibles su acción bactericida',15,'2023-10-09',4,1);
-INSERT INTO MEDICAMENTO
-VALUES (6,'Dipirona','Es un potente analgésico',18,'2023-10-17',1,2);
-INSERT INTO MEDICAMENTO
-VALUES (7,'Nefopam','Se usa para tratar el dolor moderado a intenso.',13,'2023-08-02',1,2);
-INSERT INTO MEDICAMENTO
-VALUES (8,'Loratadina','Alivia los síntomas asociados a la rinitis alérgica',25,'2023-09-30',2,2);
-
-
-
-INSERT INTO DISTRIBUIDOR
-VALUES(1,22,'2021-09-15',1,1);
-INSERT INTO DISTRIBUIDOR
-VALUES(2,23,'2021-07-25',2,2);
-INSERT INTO DISTRIBUIDOR
-VALUES(3,24,'2022-07-01',3,3);
-INSERT INTO DISTRIBUIDOR
-VALUES(4,25,'2020-02-25',4,4);
-INSERT INTO DISTRIBUIDOR
-VALUES(5,26,'2021-08-01',5,5);
-INSERT INTO DISTRIBUIDOR
-VALUES(6,27,'2019-10-20',6,6);
-INSERT INTO DISTRIBUIDOR
-VALUES(7,28,'2022-04-01',7,7);
-INSERT INTO DISTRIBUIDOR
-VALUES(8,29,'2010-01-23',8,7);
-
-
-
-
-INSERT INTO RECETA_MEDICA
-VALUES(1,'1 comprimido cada 4-6 horas, según necesidad',2,1,'2016-01-21');
-INSERT INTO RECETA_MEDICA
-VALUES(2,'Una tableta cada 4 a 6 horas, según sea necesario. La dosis no debe exceder las 5 tabletas en un período de 24 horas',1,2,'2010-02-22');
-INSERT INTO RECETA_MEDICA
-VALUES(3,'Tomarse una vez al día, con o sin alimentos',2,3,'2011-01-11');
-INSERT INTO RECETA_MEDICA
-VALUES(4,'Tomar con un vaso completo de agua 3 ó 4 veces al día para la artritis, o cada 4 a 6 horas según sea necesario',3,4,'2014-11-23');
-INSERT INTO RECETA_MEDICA
-VALUES(5,'TomaR cada 12 horas (dos veces al día) o cada 8 horas (tres veces al día) con o sin alimentos',1,5,'2019-05-08');
-INSERT INTO RECETA_MEDICA
-VALUES(6,'Tomar de 1 a 2 g cada 8 horas',1,39,'2020-08-08');
-INSERT INTO RECETA_MEDICA
-VALUES(7,'Inicialmente, 60 mg pero puede variar de 30-90 mg tid. Máx.: 300 mg diarios',1,40,'2021-08-08');
-INSERT INTO RECETA_MEDICA
-VALUES(8,'Dos cucharaditas de LORATADINA (10 ml = 10 mg) una vez al día.',1,35,'2019-01-03');
-
-
-INSERT INTO RECETA_X_MEDICAMENTOS
-VALUES(1,1,1);
-INSERT INTO RECETA_X_MEDICAMENTOS
-VALUES(2,2,2);
-INSERT INTO RECETA_X_MEDICAMENTOS
-VALUES(3,3,3);
-INSERT INTO RECETA_X_MEDICAMENTOS
-VALUES(4,4,4);
-INSERT INTO RECETA_X_MEDICAMENTOS
-VALUES(5,5,5);
-INSERT INTO RECETA_X_MEDICAMENTOS
-VALUES(6,6,6);
-INSERT INTO RECETA_X_MEDICAMENTOS
-VALUES(7,7,7);
-INSERT INTO RECETA_X_MEDICAMENTOS
-VALUES(8,8,8);
-
-INSERT INTO PRECAUCION
-VALUES (1,'Náusea, vómito, dolor epigástrico, somnolencia, ictericia, anemia hemolítica','2023-09-01',1);
-INSERT INTO PRECAUCION
-VALUES (2,'Urticaria, respiración dificultosa, hinchazón de la cara, los labios, fiebre, dolor de garganta','2023-04-01',2);
-INSERT INTO PRECAUCION
-VALUES (3,'Somnolencia, sequedad de boca, cefalea, mareo, fatiga, molestias gastrointestinales','2023-06-01',3);
-INSERT INTO PRECAUCION
-VALUES (4,'Cefalea, nerviosismo, transpiración, estreñimiento','2023-05-01',4);
-INSERT INTO PRECAUCION
-VALUES (5,'Náusea, vómitos, diarrea, dolor de cabeza','2023-10-01',5);
+INSERT INTO PRECAUCION VALUES 
+	(1,'Náusea, vómito, dolor epigástrico, somnolencia, ictericia, anemia hemolítica','2023-09-01',1),
+	(2,'Urticaria, respiración dificultosa, hinchazón de la cara, los labios, fiebre, dolor de garganta','2023-04-01',2),
+	(3,'Somnolencia, sequedad de boca, cefalea, mareo, fatiga, molestias gastrointestinales','2023-06-01',3),
+	(4,'Cefalea, nerviosismo, transpiración, estreñimiento','2023-05-01',4),
+	(5,'Náusea, vómitos, diarrea, dolor de cabeza','2023-10-01',5)
+GO
 
 
